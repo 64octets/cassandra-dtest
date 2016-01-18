9e89438084497d17419c4ae02eb5dc6be7432cc0
Sylvain Lebresne <sylvain@datastax.com>
2016-01-18 15:37:53 +0100

    Minor cleanups

--- upgrade_8099_test.py
+++ upgrade_8099_test.py
@@ -13,7 +13,7 @@ from thrift_bindings.v30 import Cassandra
 from thrift_bindings.v30.Cassandra import *
 
 class TestUpgrade8099(Tester):
-    def _setup_cluster(self):
+    def _setup_cluster(self, create_keyspace=True):
         cluster = self.cluster
 
         # Forcing cluster version on purpose
@@ -22,7 +22,11 @@ class TestUpgrade8099(Tester):
 
         [node1] = cluster.nodelist()
 
-        return self.patient_cql_connection(node1)
+        cursor = self.patient_cql_connection(node1)
+        if create_keyspace:
+            cursor.execute("CREATE KEYSPACE ks WITH replication = {'class':'SimpleStrategy', 'replication_factor': 1};")
+            cursor.execute('USE ks')
+        return cursor
 
     def _do_upgrade(self):
 
@@ -32,10 +36,8 @@ class TestUpgrade8099(Tester):
         time.sleep(.5)
         node1.stop(wait_other_notice=True)
 
-        print("Node stopped...")
         node1.set_install_dir(install_dir="/home/pcmanus/Git/cassandra") # TODO: change
         node1.start(wait_other_notice=True, wait_for_binary_proto=True)
-        print("Node restarted...")
 
         return self.patient_cql_connection(node1)
 
@@ -57,22 +59,16 @@ class TestUpgrade8099(Tester):
 
         cursor = self._setup_cluster()
 
-        print("Creating schema...")
-        cursor.execute("CREATE KEYSPACE ks WITH replication = {'class':'SimpleStrategy', 'replication_factor': 1};")
-        cursor.execute('USE ks')
         cursor.execute('CREATE TABLE t (k int, t int, v int, PRIMARY KEY (k, t))' + table_options)
 
-        print("Inserting...")
         for n in xrange(0, PARTITIONS):
             for r in xrange(0, ROWS):
                 cursor.execute("INSERT INTO t(k, t, v) VALUES (%d, %d, %d)" % (n, r, r))
 
-        print("Upgrading node...")
         cursor = self._do_upgrade()
 
         cursor.execute('USE ks')
 
-        print("Querying...")
         for n in xrange(0, PARTITIONS):
             assert_all(cursor, "SELECT * FROM t WHERE k = %d" % (n), [ [n, v, v] for v in xrange (0, ROWS)])
             assert_all(cursor, "SELECT * FROM t WHERE k = %d ORDER BY t DESC" % (n), [ [n, v, v] for v in xrange (ROWS-1, -1, -1)])
@@ -91,7 +87,6 @@ class TestUpgrade8099(Tester):
 
         self.cluster.compact()
 
-        print("Post-compaction Querying...")
         for n in xrange(0, PARTITIONS):
             assert_all(cursor, "SELECT * FROM t WHERE k = %d" % (n), [ [n, v, v] for v in xrange (0, ROWS)])
             assert_all(cursor, "SELECT * FROM t WHERE k = %d ORDER BY t DESC" % (n), [ [n, v, v] for v in xrange (ROWS-1, -1, -1)])
@@ -113,27 +108,20 @@ class TestUpgrade8099(Tester):
 
         cursor = self._setup_cluster()
 
-        print("Creating schema...")
-        cursor.execute("CREATE KEYSPACE ks WITH replication = {'class':'SimpleStrategy', 'replication_factor': 1};")
-        cursor.execute('USE ks')
         cursor.execute('CREATE TABLE t (k int PRIMARY KEY, v1 int, v2 int, v3 int, v4 int)' + table_options)
 
-        print("Inserting...")
         for n in xrange(0, PARTITIONS):
                 cursor.execute("INSERT INTO t(k, v1, v2, v3, v4) VALUES (%d, %d, %d, %d, %d)" % (n, n+1, n+2, n+3, n+4))
 
-        print("Upgrading node...")
         cursor = self._do_upgrade()
 
         cursor.execute('USE ks')
 
-        print("Querying...")
         for n in xrange(0, PARTITIONS):
             assert_one(cursor, "SELECT * FROM t WHERE k = %d" % (n), [n, n+1, n+2, n+3, n+4])
 
         self.cluster.compact()
 
-        print("Post-compaction Querying...")
         for n in xrange(0, PARTITIONS):
             assert_one(cursor, "SELECT * FROM t WHERE k = %d" % (n), [n, n+1, n+2, n+3, n+4])
 
@@ -143,29 +131,22 @@ class TestUpgrade8099(Tester):
 
         cursor = self._setup_cluster()
 
-        print("Creating schema...")
-        cursor.execute("CREATE KEYSPACE ks WITH replication = {'class':'SimpleStrategy', 'replication_factor': 1};")
-        cursor.execute('USE ks')
         cursor.execute('CREATE TABLE t (k int, s1 int static, s2 int static, t int, v1 int, v2 int, PRIMARY KEY (k, t))')
 
-        print("Inserting...")
         for n in xrange(0, PARTITIONS):
             for r in xrange(0, ROWS):
                 cursor.execute("INSERT INTO t(k, s1, s2, t, v1, v2) VALUES (%d, %d, %d, %d, %d, %d)" % (n, r, r+1, r, r, r+1))
 
-        print("Upgrading node...")
         cursor = self._do_upgrade()
 
         cursor.execute('USE ks')
 
-        print("Querying...")
         for n in xrange(0, PARTITIONS):
             assert_all(cursor, "SELECT * FROM t WHERE k = %d" % (n), [ [n, v, ROWS-1, ROWS, v, v+1] for v in xrange (0, ROWS)])
             assert_all(cursor, "SELECT * FROM t WHERE k = %d ORDER BY t DESC" % (n), [ [n, v, ROWS-1, ROWS, v, v+1] for v in xrange (ROWS-1, -1, -1)])
 
         self.cluster.compact()
 
-        print("Post-compaction Querying...")
         for n in xrange(0, PARTITIONS):
             assert_all(cursor, "SELECT * FROM t WHERE k = %d" % (n), [ [n, v, ROWS-1, ROWS, v, v+1] for v in xrange (0, ROWS)])
             assert_all(cursor, "SELECT * FROM t WHERE k = %d ORDER BY t DESC" % (n), [ [n, v, ROWS-1, ROWS, v, v+1] for v in xrange (ROWS-1, -1, -1)])
@@ -181,9 +162,6 @@ class TestUpgrade8099(Tester):
 
         cursor = self._setup_cluster()
 
-        print("Creating schema...")
-        cursor.execute("CREATE KEYSPACE ks WITH replication = {'class':'SimpleStrategy', 'replication_factor': 1};")
-        cursor.execute('USE ks')
         cursor.execute('CREATE TABLE t (k int, t int, v1 int, v2 blob, v3 set<int>, PRIMARY KEY (k, t))')
 
         # the blob is only here to make the row bigger internally so it sometimes span multiple index blocks
@@ -191,7 +169,6 @@ class TestUpgrade8099(Tester):
         for i in xrange(0, 1000):
             bigish_blob = bigish_blob + "0000"
 
-        print("Inserting...")
         for r in xrange(0, ROWS):
             cursor.execute("INSERT INTO t(k, t, v1, v2, v3) VALUES (%d, %d, %d, %s, {%d, %d})" % (0, r, r, bigish_blob, r*2, r*3))
 
@@ -205,12 +182,10 @@ class TestUpgrade8099(Tester):
         for r in xrange(1, ROWS, 4):
             cursor.execute("UPDATE t SET v3={} WHERE k=0 AND t=%d" % (r))
 
-        print("Upgrading node...")
         cursor = self._do_upgrade()
 
         cursor.execute('USE ks')
 
-        print("Querying...")
         for r in xrange(0, ROWS):
             query = "SELECT t, v1, v3 FROM t WHERE k = 0 AND t=%d%s" % (r, query_modifier)
             if (r-1) % 4 == 0:
@@ -222,7 +197,6 @@ class TestUpgrade8099(Tester):
 
         self.cluster.compact()
 
-        print("Post-compaction Querying...")
         for r in xrange(0, ROWS):
             query = "SELECT t, v1, v3 FROM t WHERE k = 0 AND t=%d%s" % (r, query_modifier)
             if (r-1) % 4 == 0:
@@ -238,47 +212,35 @@ class TestUpgrade8099(Tester):
 
         cursor = self._setup_cluster()
 
-        print("Creating schema...")
-        cursor.execute("CREATE KEYSPACE ks WITH replication = {'class':'SimpleStrategy', 'replication_factor': 1};")
-        cursor.execute('USE ks')
         cursor.execute('CREATE TABLE t (k int, t int, v1 int, v2 int, PRIMARY KEY (k, t))')
 
         cursor.execute('CREATE INDEX ON t(v1)')
 
-        print("Inserting...")
         for p in xrange(0, PARTITIONS):
             for r in xrange(0, ROWS):
                 cursor.execute("INSERT INTO t(k, t, v1, v2) VALUES (%d, %d, %d, %d)" % (p, r, r % 2, r * 2))
 
         self.cluster.flush()
 
-        print("Querying...")
         assert_all(cursor, "SELECT * FROM t WHERE v1 = 0", [[p, r, 0, r *2] for p in xrange(0, PARTITIONS) for r in xrange(0, ROWS) if r%2 == 0], ignore_order=True)
 
-        print("Upgrading node...")
         cursor = self._do_upgrade()
 
         cursor.execute('USE ks')
 
-        print("Querying...")
         assert_all(cursor, "SELECT * FROM t WHERE v1 = 0", [[p, r, 0, r *2] for p in xrange(0, PARTITIONS) for r in xrange(0, ROWS) if r%2 == 0], ignore_order=True)
 
         self.cluster.compact()
 
-        print("Querying...")
         assert_all(cursor, "SELECT * FROM t WHERE v1 = 0", [[p, r, 0, r *2] for p in xrange(0, PARTITIONS) for r in xrange(0, ROWS) if r%2 == 0], ignore_order=True)
 
-    def upgrade_with_range_tombstones(self):
+    def upgrade_with_range_tombstones_test(self):
         ROWS = 100
 
         cursor = self._setup_cluster()
 
-        print("Creating schema...")
-        cursor.execute("CREATE KEYSPACE ks WITH replication = {'class':'SimpleStrategy', 'replication_factor': 1};")
-        cursor.execute('USE ks')
         cursor.execute('CREATE TABLE t (k int, t1 int, t2 int, PRIMARY KEY (k, t1, t2))')
 
-        print("Inserting...")
         for n in xrange(0, ROWS):
             cursor.execute("INSERT INTO t(k, t1, t2) VALUES (0, 0, %d)" % n)
 
@@ -287,33 +249,26 @@ class TestUpgrade8099(Tester):
         for n in xrange(0, ROWS, 2):
             cursor.execute("INSERT INTO t(k, t1, t2) VALUES (0, 0, %d)" % n)
 
-        print("Upgrading node...")
         cursor = self._do_upgrade()
 
         cursor.execute('USE ks')
 
-        print("Querying...")
         assert_all(cursor, "SELECT * FROM t WHERE k = 0", [ [0, 0, n] for n in xrange (0, ROWS, 2)])
 
         self.cluster.compact()
 
-    def upgrade_with_range_and_collection_tombstones(self):
+    def upgrade_with_range_and_collection_tombstones_test(self):
         cursor = self._setup_cluster()
 
-        print("Creating schema...")
-        cursor.execute("CREATE KEYSPACE ks WITH replication = {'class':'SimpleStrategy', 'replication_factor': 1};")
-        cursor.execute('USE ks')
         cursor.execute('CREATE TABLE t (k text, t int, c list<int>, PRIMARY KEY (k, t))')
 
-        print("Inserting...")
         cursor.execute("INSERT INTO t(k, t, c) VALUES ('some_key', 0, %s)" % str([i for i in xrange(0, 10000)]))
 
-        print("Upgrading node...")
         cursor = self._do_upgrade()
 
         self.cluster.compact()
 
         cursor.execute('USE ks')
 
-        print("Querying...")
         assert_one(cursor, "SELECT k FROM t", ['some_key'])
+
